a4049e099f297be28a94dd4137a0141b41e13ca1
MichalGuz <michalguz@gmail.com>
2019-04-29 21:25:24 +0200

    Adding comment about method hashCode()

--- src/hash_maps/Customer.java
+++ src/hash_maps/Customer.java
@@ -19,12 +19,12 @@ public class Customer {
         return this.name.equals(c.name) && this.adress.equals(c.adress) && this.regonId.equals(c.regonId) && this.customerId.equals(c.customerId);
     }
 
+    //implementation of method for hashMap's buckets
     public int hashCode(){
         return Integer.parseInt(customerId.substring(0,3));
     }
 
     public String toString() {
-
         return name + " " + adress + " " + regonId;
     }
 }
