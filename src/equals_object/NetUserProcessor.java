b997bbc345087d47c6d326bb3a3195b3b58d436c
MichalGuz <michalguz@gmail.com>
2019-04-29 22:34:38 +0200

    Changing data of user5

--- src/equals_object/NetUserProcessor.java
+++ src/equals_object/NetUserProcessor.java
@@ -7,7 +7,7 @@ public class NetUserProcessor{
         NetUser user2 = new NetUser("Jim", "jimmy", "jimBeam", "029301" );
         NetUser user3 = new NetUser("Adam", "toto", "persona", "029302" );
         NetUser user4 = new NetUser("Nina", "ninoczka", "ladyn", "029303" );
-        NetUser user5 = new NetUser("Adam", "jj", "persona", "029302" );
+        NetUser user5 = new NetUser("Adam", "jj", "johny99", "029300" );
 
         System.out.println("user1 == user2: " + (user1 == user2));
         System.out.println("user3 == user4: " + (user3 == user4));
