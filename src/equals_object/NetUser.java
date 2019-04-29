a3832de187bef675b48c69ebe48f9c7b7a020f0d
MichalGuz <michalguz@gmail.com>
2019-04-29 21:47:44 +0200

    Adding new class NetUser

--- src/equals_object/NetUser.java
+++ src/equals_object/NetUser.java
@@ -1,3 +1,15 @@
 package equals_object;
 
+public class NetUser {
+    String name;
+    String nick;
+    String login;
+    String numberId;
 
+    public NetUser(String name, String nick, String login, String numberId) {
+        this.name = name;
+        this.nick = nick;
+        this.login = login;
+        this.numberId = numberId;
+    }
+}
