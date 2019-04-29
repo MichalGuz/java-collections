7e3be02456b827a197b97b70a741d6a4b41bbfc1
MichalGuz <michalguz@gmail.com>
2019-04-29 22:32:09 +0200

    Adding class NetUserProcessor and users' comparing

---
+++ src/equals_object/NetUserProcessor.java
@@ -0,0 +1,16 @@
+package equals_object;
+
+public class NetUserProcessor{
+
+    public static void main (String[] args) throws java.lang.Exception {
+        NetUser user1 = new NetUser("John", "jj", "johny99", "029300" );
+        NetUser user2 = new NetUser("Jim", "jimmy", "jimBeam", "029301" );
+        NetUser user3 = new NetUser("Adam", "toto", "persona", "029302" );
+        NetUser user4 = new NetUser("Nina", "ninoczka", "ladyn", "029303" );
+        NetUser user5 = new NetUser("Adam", "jj", "persona", "029302" );
+
+        System.out.println("user1 == user2: " + (user1 == user2));
+        System.out.println("user3 == user4: " + (user3 == user4));
+        System.out.println("user1 == user5: " + (user1 == user5));
+    }
+}
