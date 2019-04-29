8794c9810a6e5a9675fd57a06786d48755dd2464
MichalGuz <michalguz@gmail.com>
2019-04-29 22:51:18 +0200

    Adding new objects NetuserEvo

--- src/equals_object/NetUserProcessor.java
+++ src/equals_object/NetUserProcessor.java
@@ -12,5 +12,12 @@ public class NetUserProcessor{
         System.out.println("user1 == user2: " + (user1 == user2));
         System.out.println("user3 == user4: " + (user3 == user4));
         System.out.println("user1 == user5: " + (user1 == user5));
+
+        NetUserEvo user11 = new NetUserEvo("John", "jj", "johny99", "029300" );
+        NetUserEvo user12 = new NetUserEvo("Jim", "jimmy", "jimBeam", "029301" );
+        NetUserEvo user13 = new NetUserEvo("Adam", "toto", "persona", "029302" );
+        NetUserEvo user14 = new NetUserEvo("Nina", "ninoczka", "ladyn", "029303" );
+        NetUserEvo user15 = new NetUserEvo("Adam", "jj", "johny99", "029300" );
+
     }
 }
