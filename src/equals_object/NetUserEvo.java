f98582f26a395d19096b8475c5bbbf31cb0bd1c8
MichalGuz <michalguz@gmail.com>
2019-04-29 22:41:57 +0200

    Adding body class NetUserEvo

--- src/equals_object/NetUserEvo.java
+++ src/equals_object/NetUserEvo.java
@@ -1,4 +1,15 @@
 package equals_object;
 
 public class NetUserEvo {
+    String name;
+    String nick;
+    String login;
+    String numberId;
+
+    public NetUserEvo(String name, String nick, String login, String numberId) {
+        this.name = name;
+        this.nick = nick;
+        this.login = login;
+        this.numberId = numberId;
+    }
 }
