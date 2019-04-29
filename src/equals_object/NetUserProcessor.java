e39e4363af889d921efe06e827d2aee2b32b6e5e
MichalGuz <michalguz@gmail.com>
2019-04-29 23:03:57 +0200

    Adding method equals() and overriting it

--- src/equals_object/NetUserProcessor.java
+++ src/equals_object/NetUserProcessor.java
@@ -19,6 +19,7 @@ public class NetUserProcessor{
         NetUserEvo user14 = new NetUserEvo("Marc", "marco", "marcopolo", "029314" );
         NetUserEvo user15 = new NetUserEvo("Igor", "iggy", "iggy1", "029311" );
 
+        //method equal() finds double objects
         System.out.println("user11 == user12: " + (user11.equals(user12)));
         System.out.println("user13 == user14: " + (user13.equals(user14)));
         System.out.println("user11 == user15: " + (user11.equals(user15)));
