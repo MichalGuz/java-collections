007b51bd23b5f7de57ee76ec3fe4e5d711eb9a55
MichalGuz <michalguz@gmail.com>
2019-04-30 18:03:32 +0200

    Adding new class Student

---
+++ src/hashmap_grades/Student.java
@@ -0,0 +1,13 @@
+package hashmap_grades;
+
+public class Student {
+    String name;
+    String surname;
+    String numberID;
+
+    public Student(String name, String surname, String numberID) {
+        this.name = name;
+        this.surname = surname;
+        this.numberID = numberID;
+    }
+}
