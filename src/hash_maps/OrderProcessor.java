4979498d5bd687896419153b0ea6a5687672f53c
MichalGuz <michalguz@gmail.com>
2019-04-30 18:45:16 +0200

    Creating objects Order

--- src/hash_maps/OrderProcessor.java
+++ src/hash_maps/OrderProcessor.java
@@ -8,6 +8,12 @@ public class OrderProcessor {
         Customer customer4 = new Customer("Kowalski Instalacje", "Szczecin", "834164173", "70-0004" );
         Customer customer5 = new Customer("ZTD Poznań", "Poznań", "794375601", "60-0024" );
 
+        Order order1= new Order(1000.0, 400.0, 0.0);
+        Order order2= new Order(0.0, 450.0, 20.0);
+        Order order3= new Order(0.0, 0.0, 100.0);
+        Order order4= new Order(1500.0, 5000.0, 600.0);
+
+
 
     }
 }
