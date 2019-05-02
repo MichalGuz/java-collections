5ab2e6f79e3982e1d2c630289bf8fb8b915df34e
MichalGuz <michalguz@gmail.com>
2019-05-02 15:05:17 +0200

    Refactoring of variables in class Contact

--- src/hash_maps/Contact.java
+++ src/hash_maps/Contact.java
@@ -1,19 +1,17 @@
 package hash_maps;
 
 public class Contact {
-    Double product1;
-    Double product2;
-    Double product3;
+    String contact1;
+    String contact2;
+    String contact3;
 
-    public Contact(Double product1, Double product2, Double product3){
-        this.product1 = product1;
-        this.product2 = product2;
-        this.product3 = product3;
+    public Contact(String contact1, String contact2, String contact3){
+        this.contact1 = contact1;
+        this.contact2 = contact2;
+        this.contact3 = contact3;
     }
 
-    public String toString() {
-        return product1 + " + " + product2 + " + " + product3;
-    }
+   // public String toString() {        return contact1 + " + " + contact2 + " + " + contact3;    }
 
 
 }
