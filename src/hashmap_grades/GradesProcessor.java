7097f8733df8b10c33e402eb0c98b08aefc835f2
MichalGuz <michalguz@gmail.com>
2019-05-03 12:28:26 +0200

    Creating new class GradeProcessor and new objects Student

---
+++ src/hashmap_grades/GradesProcessor.java
@@ -0,0 +1,25 @@
+package hashmap_grades;
+
+import com.sun.javafx.sg.prism.GrowableDataBuffer;
+
+import java.util.HashMap;
+
+public class GradesProcessor {
+    public static void main (String[] args) {
+        Student s1 = new Student("Ada", "Adamska", "01");
+        Student s2 = new Student("Adam", "Adamski", "02");
+        Student s3 = new Student("Bartosz", "Adamiak", "03");
+        Student s4 = new Student("Bianka", "Bystry", "04");
+        Student s5 = new Student("Cecylia", "Bystrzycka", "05");
+        Student s6 = new Student("Cezary", "Cacek", "06");
+        Student s10 = new Student("Marek", "Malinowski", "10");
+        Student s11 = new Student("Anna", "Mikulski", "11");
+        Student s12 = new Student("Jan", "Mutko", "12");
+        Student s20 = new Student("Izabela", "Orska", "20");
+        Student s21 = new Student("Julia", "Polak", "21");
+        Student s22 = new Student("Agnieszka", "Prorok", "22");
+        Student s30 = new Student("Roman","Sulima", "30");
+        Student s33 = new Student("Klaudia", "Szewczyk", "33");
+
+    }
+}
