b9de3202bbdf729d320a8a2d7b20c98c8d4102ad
MichalGuz <michalguz@gmail.com>
2019-05-03 11:11:49 +0200

    New class Grade with instance variables

---
+++ src/hashmap_grades/Grade.java
@@ -0,0 +1,19 @@
+package hashmap_grades;
+
+public class Grade {
+    int math;
+    int biology;
+    int physics;
+    int computerScience;
+    int economics;
+    int english;
+    int german;
+    int french;
+    int chemistry;
+    int geography;
+    int history;
+    int logic;
+    int art;
+    int music;
+    int games;
+}
