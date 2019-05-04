7ebfae6da267d6c79b9df4ee7dbba3ed103b85d2
MichalGuz <michalguz@gmail.com>
2019-05-04 18:25:55 +0200

    Create constructor in class User with LocalDate

--- src/hashSet_users/User.java
+++ src/hashSet_users/User.java
@@ -7,4 +7,13 @@ public class User {
     String surname;
     String nick;
     LocalDate dateOfRegisty;
+
+    public User(String name, String surname, String nick, int yearOfRegistry, int monthOfRegistry, int dayOfRegistry) {
+        this.name = name;
+        this.surname = surname;
+        this.nick = nick;
+        this.dateOfRegisty = LocalDate.of(yearOfRegistry, monthOfRegistry, dayOfRegistry);
+    }
+
+
 }
