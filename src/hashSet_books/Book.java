5362b7afa5b2ea3d54bf21d4f94d2926fb03e008
MichalGuz <michalguz@gmail.com>
2019-05-06 00:11:47 +0200

    Creation of method of getters in class Book

--- src/hashSet_books/Book.java
+++ src/hashSet_books/Book.java
@@ -5,15 +5,26 @@ public class Book {
     public String title;
     public int yearOfRelease;
 
-    public Book(String author, String title, int yearOfRelease){
+    public Book(String author, String title, int yearOfRelease) {
         this.author = author;
         this.title = title;
         this.yearOfRelease = yearOfRelease;
     }
 
     @Override
-    public int hashCode(){
+    public int hashCode() {
         return yearOfRelease;
     }
 
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
 }
