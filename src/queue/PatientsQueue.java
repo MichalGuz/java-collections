a64636d809171090aee5e550ff0a99ca2946446a
MichalGuz <michalguz@gmail.com>
2019-05-05 22:51:14 +0200

    Creation objects of class Patient in class PatientsQueue

--- src/queue/PatientsQueue.java
+++ src/queue/PatientsQueue.java
@@ -2,6 +2,8 @@ package queue;
 
 public class PatientsQueue {
     public static void main(String[] args) {
-
+        Patient patient1 = new Patient("Bob", "Hopkins", 66, "195311240143");
+        Patient patient2 = new Patient("Sarah", "Atkins", 54, "196502140023");
+        Patient patient3 = new Patient("Lisa", "Morgan", 34, "19850906015");
     }
 }
