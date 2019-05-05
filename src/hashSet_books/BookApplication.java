d99eb88c3ed15e22c971add822430829913ce564
MichalGuz <michalguz@gmail.com>
2019-05-06 00:29:58 +0200

    Creation of class BookApplication with new objects of class Book

---
+++ src/hashSet_books/BookApplication.java
@@ -0,0 +1,14 @@
+package hashSet_books;
+
+import java.util.HashSet;
+
+public class BookApplication {
+    public static void main(String[] args) {
+        Book book1 = new Book("Umberto Eco", "Imię Róży", 1990);
+        Book book2 = new Book("Italo Calvino", "Wicehrabia przepołowiony", 1992);
+        Book book3 = new Book("Jose Luis Borges", "Fikcje", 1988);
+        Book book4 = new Book("Witold Gombrowicz", "Kosmos",1970);
+        Book book5 = new Book("Szczepan Twardoch", "Morfina", 2012);
+        Book book6 = new Book("Michel Houellebecq", "Cząstki elementarne", 2000);
+    }
+}
