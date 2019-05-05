7a9cb3e0c8d8789bc7a81aaf52b40ae5ce07e4af
MichalGuz <michalguz@gmail.com>
2019-05-05 22:11:49 +0200

    Create constructor in class Patient

--- src/queue/Patient.java
+++ src/queue/Patient.java
@@ -5,4 +5,11 @@ public class Patient {
     public String surname;
     public int age;
     public int medicalInsuranceNumber;
+
+    public Patient(String name, String surname, int age, int medicalInsuranceNumber){
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.medicalInsuranceNumber = medicalInsuranceNumber;
+    }
 }
