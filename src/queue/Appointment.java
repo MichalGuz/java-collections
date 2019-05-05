feb29d826aecfeb8ac165032f8e446d36a4574bc
MichalGuz <michalguz@gmail.com>
2019-05-05 22:20:58 +0200

    Creation constructor in class Appoinment

--- src/queue/Appointment.java
+++ src/queue/Appointment.java
@@ -6,4 +6,8 @@ public class Appointment {
     public Patient patient;
     public LocalDate dateOfAppointment;
 
+    public Appointment(Patient patient, int year, int month, int day){
+        this.patient = patient;
+        this.dateOfAppointment = LocalDate.of(year, month, day);
+    }
 }
