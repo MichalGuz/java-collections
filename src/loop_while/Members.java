4b2b60cde701dd7fdfe05717027c416f9db21502
MichalGuz <michalguz@gmail.com>
2019-05-06 23:47:48 +0200

    Creation of constructor of class Members

--- src/loop_while/Members.java
+++ src/loop_while/Members.java
@@ -6,4 +6,12 @@ public class Members {
     String name;
     String surname;
     LocalDate birthDate;
+
+    public Members(String name, String surname, int year, int month, int day) {
+        this.name = name;
+        this.surname = surname;
+        this.birthDate = LocalDate.of(year, month, day);
+    }
+
+
 }
