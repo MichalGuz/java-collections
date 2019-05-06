28928bf9181d3127d55642f048de262e7edf6960
MichalGuz <michalguz@gmail.com>
2019-05-06 18:00:36 +0200

    Adding second variable and creation of constructor in class Item

--- src/queue_stack_clipboard/Item.java
+++ src/queue_stack_clipboard/Item.java
@@ -2,5 +2,10 @@ package queue_stack_clipboard;
 
 public class Item {
     String item;
+    String numberOfItem;
 
+    public Item(String item, String numberOfItem){
+        this.item = item;
+        this.numberOfItem = numberOfItem;
+    }
 }
