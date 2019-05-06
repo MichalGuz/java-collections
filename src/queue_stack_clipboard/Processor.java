c49d333faff87d8e5be25904173af1aa9709ff34
MichalGuz <michalguz@gmail.com>
2019-05-06 18:30:20 +0200

    Creation of objetcs of class Item

--- src/queue_stack_clipboard/Processor.java
+++ src/queue_stack_clipboard/Processor.java
@@ -2,6 +2,13 @@ package queue_stack_clipboard;
 
 public class Processor {
     public static void main(String[] args) {
+        Item item0 = new Item("first text to copy", "0");
+        Item item1 = new Item("second text to copy", "1");
+        Item item2 = new Item("third text to copy", "2");
+        Item item3 = new Item("fourth text to copy", "3");
+        Item item4 = new Item("fifth text to copy", "4");
+
+
 
     }
 }
