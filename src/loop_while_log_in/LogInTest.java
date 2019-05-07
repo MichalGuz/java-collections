e1725d3d91c45f5a8aaafa4a3e8decd44be84b44
MichalGuz <michalguz@gmail.com>
2019-05-07 19:05:57 +0200

    Create new object of class Random

--- src/loop_while_log_in/LogInTest.java
+++ src/loop_while_log_in/LogInTest.java
@@ -1,8 +1,12 @@
 package loop_while_log_in;
 
+import java.util.Random;
+
 public class LogInTest {
     public static void main(String[] args) {
         boolean isLogged = false;
         int loopCounter = 0;
+        Random randomGenerator = new Random();
+
     }
 }
