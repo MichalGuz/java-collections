cfe23b0b490085236e0595fa8babc4d233fbb578
MichalGuz <michalguz@gmail.com>
2019-05-08 00:43:50 +0200

    Create new object of class Random and new loop for. Loop fills big collection of numbers

--- src/lists/BigList.java
+++ src/lists/BigList.java
@@ -6,5 +6,9 @@ import java.util.Random;
 public class BigList {
     public void bigListCreator(){
         ArrayList<Integer> bigList = new ArrayList<>();
-      }
+        Random random = new Random();
+        for(int i=0; i<1500000; i++){
+            bigList.add(random.nextInt(10000));
+        }
+    }
 }
