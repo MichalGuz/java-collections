ddf3f413dec450315f525a467f058e26e6fb7310
MichalGuz <michalguz@gmail.com>
2019-05-08 00:40:46 +0200

    Create new ArrayList

--- src/lists/BigList.java
+++ src/lists/BigList.java
@@ -1,7 +1,10 @@
 package lists;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class BigList {
     public void bigListCreator(){
-
-    }
+        ArrayList<Integer> bigList = new ArrayList<>();
+      }
 }
