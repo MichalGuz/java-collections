439ab08981f9e4599f5bc3b4debee02a6ac59420
MichalGuz <michalguz@gmail.com>
2019-05-07 20:08:20 +0200

    Create new objects of class Random for increasing values both of sums

--- src/loop_while_sum/Sum.java
+++ src/loop_while_sum/Sum.java
@@ -1,8 +1,12 @@
 package loop_while_sum;
 
+import java.util.Random;
+
 public class Sum {
     public static void main(String[] args) {
         int sumA = 1000;
         int sumB = 0;
-      }
+        Random randomA = new Random();
+        Random randomB = new Random();
+    }
 }
