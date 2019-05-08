cde7e21c92185398d27ec0e2414af1f723e13d9a
MichalGuz <michalguz@gmail.com>
2019-05-09 01:06:37 +0200

    Create loop for to fill the list with objects and use the method printEven()

--- src/list_as_parameter/ListAsParameter.java
+++ src/list_as_parameter/ListAsParameter.java
@@ -1,4 +1,16 @@
 package list_as_parameter;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class ListAsParameter {
+    public static void main(String[] args) {
+        ArrayList<Integer> theList = new ArrayList<>();
+        Random randomGenerator = new Random();
+        for (int i = 0; i  < 200; i++){
+            theList.add(randomGenerator.nextInt(100));
+            OddEliminator oddEliminator  = new OddEliminator();
+            oddEliminator.printEven(theList);
+        }
+    }
 }
