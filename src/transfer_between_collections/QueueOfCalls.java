032ded19a964e70b9abd7f4624e2264f2298d48f
MichalGuz <michalguz@gmail.com>
2019-05-10 00:33:36 +0200

    Create new class with main() method (and new queue in it)

---
+++ src/transfer_between_collections/QueueOfCalls.java
@@ -0,0 +1,9 @@
+package transfer_between_collections;
+
+import java.util.ArrayDeque;
+
+public class QueueOfCalls {
+    public static void main(String[] args) {
+        ArrayDeque<String> callQueue = new ArrayDeque<String>();
+    }
+}
