42350450548c0454adacb787b0b77a9b2d0c958a
MichalGuz <michalguz@gmail.com>
2019-05-10 00:40:30 +0200

    Fill the collection queue

--- src/transfer_between_collections/QueueOfCalls.java
+++ src/transfer_between_collections/QueueOfCalls.java
@@ -5,5 +5,8 @@ import java.util.ArrayDeque;
 public class QueueOfCalls {
     public static void main(String[] args) {
         ArrayDeque<String> callQueue = new ArrayDeque<String>();
+        for (int i=0; i < 5; i++){
+            callQueue.offer("The first call number " + (i + 1));
+        }
     }
 }
