28bdf7cfef42aac6b7d1a016da6fd44df745fb55
MichalGuz <michalguz@gmail.com>
2019-05-11 18:44:33 +0200

    Create a new collection type MyObject and loop for to fill the collection with random numbers)

--- src/loop_while_duplicates/DuplicateDetector.java
+++ src/loop_while_duplicates/DuplicateDetector.java
@@ -1,7 +1,14 @@
 package loop_while_duplicates;
 
+import java.util.LinkedList;
+import java.util.Random;
+
 public class DuplicateDetector {
     public static void main(String[] args) {
-
+        LinkedList<MyObject> numbers = new LinkedList<MyObject>();
+        Random randomGenerator = new Random();
+        for(int i = 0; i < 100; i++){
+            numbers.add(new MyObject(randomGenerator.nextInt(100)));
+        }
     }
 }
