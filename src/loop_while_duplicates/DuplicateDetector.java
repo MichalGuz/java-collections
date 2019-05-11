3eaeadbe1bf5eb4c82d129d2ce8f8a0a31c6efe0
MichalGuz <michalguz@gmail.com>
2019-05-11 22:12:35 +0200

    Add in loop body condition if, that checks equality of objects by comparing checkedObjects with every element of linkedList

--- src/loop_while_duplicates/DuplicateDetector.java
+++ src/loop_while_duplicates/DuplicateDetector.java
@@ -8,15 +8,17 @@ public class DuplicateDetector {
     public static void main(String[] args) {
         LinkedList<MyObject> numbers = new LinkedList<MyObject>();
         Random randomGenerator = new Random();
-        for(int i = 0; i < 100; i++){
+        for (int i = 0; i < 100; i++) {
             numbers.add(new MyObject(randomGenerator.nextInt(100)));
         }
 
         Iterator<MyObject> iterator = numbers.iterator();
-        while(iterator.hasNext()){
+        while (iterator.hasNext()) {
             MyObject checkedObject = iterator.next();
-            for(int p=0; p < numbers.size(); p++){
+            for (int p = 0; p < numbers.size(); p++) {
+                if (numbers.get(p).hashCode() != checkedObject.hashCode()) {
 
+                }
             }
         }
     }
