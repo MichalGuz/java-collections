702dd7a1ad7c2a6522f97bd34b38bb23c8f73c87
MichalGuz <michalguz@gmail.com>
2019-05-11 18:52:01 +0200

    Create loop while to check duplicates; for first - iterator checks if there is another object

--- src/loop_while_duplicates/DuplicateDetector.java
+++ src/loop_while_duplicates/DuplicateDetector.java
@@ -1,5 +1,6 @@
 package loop_while_duplicates;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Random;
 
@@ -9,6 +10,11 @@ public class DuplicateDetector {
         Random randomGenerator = new Random();
         for(int i = 0; i < 100; i++){
             numbers.add(new MyObject(randomGenerator.nextInt(100)));
+        }
+
+        Iterator<MyObject> iterator = numbers.iterator();
+        while(iterator.hasNext()){
+
         }
     }
 }
