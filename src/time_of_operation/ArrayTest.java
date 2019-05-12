a694ed421e6a7ed4dabf0e82eac682f80e5bf630
MichalGuz <michalguz@gmail.com>
2019-05-12 23:49:51 +0200

    Create new ArrayList collection in class ArrayTest. Fill this collection with 10000 numbers

--- src/time_of_operation/ArrayTest.java
+++ src/time_of_operation/ArrayTest.java
@@ -1,7 +1,12 @@
 package time_of_operation;
 
+import java.util.ArrayList;
+
 public class ArrayTest {
     public static void main(String[] args) {
-
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for(int i = 0; i < 10000; i++){
+            numbers.add(i);
+        }
     }
 }
