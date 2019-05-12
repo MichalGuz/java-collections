38682afb98e0c40c41d175d7ad3e593ab2ab2601
MichalGuz <michalguz@gmail.com>
2019-05-12 23:59:50 +0200

    correction of the error - move printing method from loop for body

--- src/time_of_operation/ArrayTest.java
+++ src/time_of_operation/ArrayTest.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 public class ArrayTest {
     public static void main(String[] args) {
         ArrayList<Integer> numbers = new ArrayList<>();
-        for(int i = 0; i < 10000; i++){
+        for(int i = 0; i < 10000; i++) {
             numbers.add(i);
-
-            System.out.println("Size of collection: " + numbers.size() + " elements.");
         }
+
+        System.out.println("Size of collection 'numbers': " + numbers.size() + " elements.");
     }
 }
