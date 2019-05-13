2892f63f2ced791cb5504162c0925184a36afed8
MichalGuz <michalguz@gmail.com>
2019-05-14 01:22:36 +0200

    Create new ArrayList

--- src/time_of_operation/BigArrayList.java
+++ src/time_of_operation/BigArrayList.java
@@ -1,8 +1,10 @@
 package time_of_operation;
 
+import java.util.ArrayList;
+
 public class BigArrayList {
 
     public void bigArrayListProcessor(){
-
+        ArrayList<Integer> numbers = new ArrayList<>();
     }
 }
