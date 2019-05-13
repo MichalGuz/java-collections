85fd1f9084d2e537c41c342d306f1f43b1b39007
MichalGuz <michalguz@gmail.com>
2019-05-14 01:13:09 +0200

    Refactor class - rename the class

--- src/time_of_operation/TimeDiffrences.java
+++
@@ -1,6 +0,0 @@
-package time_of_operation;
-
-public class TimeDiffrences {
-    long start;
-    long end;
-}
---
+++ src/time_of_operation/TimeOfOperation.java
@@ -0,0 +1,8 @@
+package time_of_operation;
+
+public class TimeOfOperation {
+    long start;
+    long end;
+
+//    long calculateDiffrence
+}
