86c0774bb3d271e3dca30720c78557ba4673abd9
MichalGuz <michalguz@gmail.com>
2019-05-15 00:24:04 +0200

    Overriding method createCollection()

--- src/time_of_operation/BigArrayList.java
+++ src/time_of_operation/BigArrayList.java
@@ -5,4 +5,15 @@ import java.util.ArrayList;
 public class BigArrayList implements CollectionProcessor {
     ArrayList<Integer> numbers;
     long differenceOfTime;
+
+    @Override
+    public void createCollection() {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 1000000; i++) {
+            numbers.add(i);
+        }
+
+        System.out.println("Size of collection 'numbers': " + numbers.size() + " elements.");
+    }
+
 }
