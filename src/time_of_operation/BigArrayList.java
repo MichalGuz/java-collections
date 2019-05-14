d82a14757331432e8997af151e0a2784bec65fdc
MichalGuz <michalguz@gmail.com>
2019-05-15 00:35:36 +0200

    Overriding method implements interface CollectionProcessor

--- src/time_of_operation/BigArrayList.java
+++ src/time_of_operation/BigArrayList.java
@@ -15,6 +15,7 @@ public class BigArrayList implements CollectionProcessor {
 
         System.out.println("Size of collection 'numbers': " + numbers.size() + " elements.");
     }
+
     @Override
     public long timeOfAddFirst() {
         long start1 = System.currentTimeMillis();
@@ -23,5 +24,27 @@ public class BigArrayList implements CollectionProcessor {
         return differenceOfTime = end1 - start1;
     }
 
+    @Override
+    public long timeOfAddLast() {
+        long start2 = System.currentTimeMillis();
+        numbers.add(numbers.size());
+        long end2 = System.currentTimeMillis();
+        return differenceOfTime = end2 - start2;
+    }
 
+    @Override
+    public long timeOfRemoveFirst() {
+        long start3 = System.currentTimeMillis();
+        numbers.remove(0);
+        long end3 = System.currentTimeMillis();
+        return differenceOfTime = end3 - start3;
+    }
+
+    @Override
+    public long timeOfRemoveLast() {
+        long start4 = System.currentTimeMillis();
+        numbers.remove(numbers.size() - 1);
+        long end4 = System.currentTimeMillis();
+        return differenceOfTime = end4 - start4;
+    }
 }
