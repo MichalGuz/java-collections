f0483616e401c2129719b39e03f0e21cc5f6fda7
MichalGuz <michalguz@gmail.com>
2019-05-15 00:44:37 +0200

    Create method void in TimeOperation program and create body of class TimeComparison for execute the operation

--- src/time_of_operation/TimeComparison.java
+++ src/time_of_operation/TimeComparison.java
@@ -1,6 +1,14 @@
 package time_of_operation;
 
 public class TimeComparison {
+    public static void main(String[] args) {
 
+        BigArrayList list = new BigArrayList();
+        list.createCollection();
+        list.timeOfAddFirst();
+        list.timeOfAddLast();
+        list.timeOfRemoveFirst();
+        list.timeOfRemoveLast();
+        list.print();
     }
 }
