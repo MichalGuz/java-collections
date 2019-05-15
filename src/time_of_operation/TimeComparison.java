4602361b4b689cc2752edecdce2d80ede4c3b654
MichalGuz <michalguz@gmail.com>
2019-05-16 00:44:32 +0200

    Create collection of 1000000 items and  execute methods of testing time of operations on this collection

--- src/time_of_operation/TimeComparison.java
+++ src/time_of_operation/TimeComparison.java
@@ -1,7 +1,24 @@
 package time_of_operation;
 
+import java.util.ArrayList;
+
 public class TimeComparison {
     public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 1000000; i++) {
+            numbers.add(i);
+        }
+
+        ArrayListProcessor processor = new ArrayListProcessor();
+        processor.timeOfAddFirstToArrayList(numbers);
+        processor.timeOfAddLastToArrayList(numbers);
+        processor.timeOfRemoveFirstFromArrayList(numbers);
+        processor.timeOfRemoveLastFromArrayList(numbers);
+
+        ArrayListProcessor arrayListProcessor = new ArrayListProcessor();
+
+        ArrayListPrintTime arrayListPrintTime = new ArrayListPrintTime();
+        arrayListPrintTime.printArrayList(arrayListProcessor,numbers);
 
     }
 }
