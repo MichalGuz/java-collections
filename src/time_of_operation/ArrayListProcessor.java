52b17576454bd3b5ca5c06a1cc0efc2a89a2f14b
MichalGuz <michalguz@gmail.com>
2019-05-15 23:50:53 +0200

    Change the code, rename class for using it to any kind of ArrayList (big, medium, small)

---
+++ src/time_of_operation/ArrayListProcessor.java
@@ -0,0 +1,45 @@
+package time_of_operation;
+
+import java.util.ArrayList;
+
+public class ArrayListProcessor {
+    ArrayListProcessor arrayListProcessor;
+    long differenceOfTime;
+
+    public long timeOfAddFirstToArrayList(ArrayList<Integer> list) {
+        long start1 = System.currentTimeMillis();
+        list.add(0,1);
+        long end1 = System.currentTimeMillis();
+        return differenceOfTime = end1 - start1;
+    }
+
+    public long timeOfAddLastToArrayList(ArrayList<Integer> list) {
+        long start2 = System.currentTimeMillis();
+        list.add(list.size());
+        long end2 = System.currentTimeMillis();
+        return differenceOfTime = end2 - start2;
+    }
+
+    public long timeOfRemoveFirstFromArrayList(ArrayList<Integer> list) {
+        long start3 = System.currentTimeMillis();
+        list.remove(0);
+        long end3 = System.currentTimeMillis();
+        return differenceOfTime = end3 - start3;
+    }
+
+    public long timeOfRemoveLastFromArrayList(ArrayList<Integer> list) {
+        long start4 = System.currentTimeMillis();
+        list.remove(list.size() - 1);
+        long end4 = System.currentTimeMillis();
+        return differenceOfTime = end4 - start4;
+    }
+
+    public void printArrayList(ArrayList<Integer> list) {
+        System.out.println("[Big ArrayList] = " + list.size() + " elements.");
+        System.out.println("Time of adding new element at the beginning of collection [ms]:" + arrayListProcessor.timeOfAddFirstToArrayList(list));
+        System.out.println("Time of adding new element at the end of collection [ms]:" + arrayListProcessor.timeOfAddLastToArrayList(list));
+        System.out.println("Time of removing an element from the beginning of collection [ms]:" + arrayListProcessor.timeOfRemoveFirstFromArrayList(list));
+        System.out.println("Time of removing an element from the end of collection [ms]:" + arrayListProcessor.timeOfRemoveLastFromArrayList(list));
+    }
+
+}
