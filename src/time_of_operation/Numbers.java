0caca6d92a69c5ce31eb7bccbb54a5c4c848d734
MichalGuz <michalguz@gmail.com>
2019-05-16 23:55:47 +0200

    Create constructor in class Numbers

--- src/time_of_operation/Numbers.java
+++ src/time_of_operation/Numbers.java
@@ -3,4 +3,9 @@ package time_of_operation;
 public class Numbers {
     Integer number;
     Integer parameter;
+
+    public Numbers(Integer number, Integer parameter) {
+        this.number = number;
+        this.parameter = parameter;
+    }
 }
