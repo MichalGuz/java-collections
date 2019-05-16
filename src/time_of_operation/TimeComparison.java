c172d109af321fd0cf02c53f851d4c4fc39a8e67
MichalGuz <michalguz@gmail.com>
2019-05-16 23:11:41 +0200

    Execute methods of class LinkedListProcessor and LinkListPrintTime with argument: collection bigLinkedList

--- src/time_of_operation/TimeComparison.java
+++ src/time_of_operation/TimeComparison.java
@@ -33,15 +33,25 @@ public class TimeComparison {
 
         arrayListPrintTime.printArrayList(arrayListProcessor,numbers2);
 
-//        LinkedList<Integer> bigLinkedList = new LinkedList<>();
-//        for (int i = 0; i < 1000000; i++ ) {
-//            bigLinkedList.add(i);
-//        }
-//
-//        LinkedListProcessor linkedListProcessor = new LinkedListProcessor();
-//
-//        System.out.println("\n");
-//
+
+        LinkedList<Integer> bigLinkedList = new LinkedList<>();
+        for (int i = 0; i < 1000000; i++ ) {
+            bigLinkedList.add(i);
+        }
+
+        LinkedListProcessor linkedListProcessor = new LinkedListProcessor();
+
+        System.out.println("\n");
+
+        linkedListProcessor.timeOfAddFirstToLinkedList(bigLinkedList);
+        linkedListProcessor.timeOfAddLastToLinkedList(bigLinkedList);
+        linkedListProcessor.timeOfRemoveFirstFromLinkedList(bigLinkedList);
+        linkedListProcessor.timeOfRemoveLastFromLinkedList(bigLinkedList);
+
+        LinkedListPrintTime linkedListPrintTime = new LinkedListPrintTime();
+        linkedListPrintTime.printLinkedListTime(linkedListProcessor, bigLinkedList);
+
+
 
     }
 }
