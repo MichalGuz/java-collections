258f6932e746f846fccb8eef809d8073d992c9c3
MichalGuz <michalguz@gmail.com>
2019-05-16 20:04:20 +0200

    Create method for checking time of operation: add first element to LinkedList

--- src/time_of_operation/LinkedListProcessor.java
+++ src/time_of_operation/LinkedListProcessor.java
@@ -1,7 +1,18 @@
 package time_of_operation;
 
+import java.util.LinkedList;
+
 public class LinkedListProcessor {
     LinkedListProcessor linkedListProcessor;
 
     long differenceOfTime;
+
+    public long timeOfAddFirstToLinkedList(LinkedList<Integer> linkedList){
+        long start1 = System.currentTimeMillis();
+        linkedList.add(0);
+        long end1 = System.currentTimeMillis();
+        return differenceOfTime = end1 - start1;
+    }
+
+
 }
