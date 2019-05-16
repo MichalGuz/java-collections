b81a588c9b9567553502f547cbcd843c06ab9d4a
MichalGuz <michalguz@gmail.com>
2019-05-17 00:38:57 +0200

    Create class Value with variables, vonstructor and Overriden method hashCode()

---
+++ src/time_of_operation/Values.java
@@ -0,0 +1,14 @@
+package time_of_operation;
+
+public class Values {
+    Integer value;
+
+    public Values(Integer value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString(){
+        return value + ";";
+    }
+}
