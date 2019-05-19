662504c607167ce0905770fb30c79b97f006a8de
MichalGuz <michalguz@gmail.com>
2019-05-19 22:46:48 +0200

    Create loop for filling collection with objects

--- src/task_with_letters/LettersArrayDeque.java
+++ src/task_with_letters/LettersArrayDeque.java
@@ -3,9 +3,11 @@ package task_with_letters;
 import java.util.ArrayDeque;
 
 public class LettersArrayDeque {
-    public void createArrayDequeWithLetters(){
+    public void createArrayDequeWithLetters() {
         ArrayDeque<Letters> letters = new ArrayDeque();
         String one = "A";
 
+        for (int p = 0; p < 50; p++) {
+        }
     }
 }
