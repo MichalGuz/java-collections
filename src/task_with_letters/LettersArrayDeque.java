99f437a95b360c5f4090fd426bde70e303804843
MichalGuz <michalguz@gmail.com>
2019-05-19 22:45:07 +0200

    Create new collection letters and inicialise variable with value of letter A

--- src/task_with_letters/LettersArrayDeque.java
+++ src/task_with_letters/LettersArrayDeque.java
@@ -1,7 +1,11 @@
 package task_with_letters;
 
+import java.util.ArrayDeque;
+
 public class LettersArrayDeque {
     public void createArrayDequeWithLetters(){
+        ArrayDeque<Letters> letters = new ArrayDeque();
+        String one = "A";
 
     }
 }
