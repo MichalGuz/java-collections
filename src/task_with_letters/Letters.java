f2ba1c758904bd5dab434e14a00d3d77b55f416e
MichalGuz <michalguz@gmail.com>
2019-05-19 21:21:04 +0200

    Create getter methods

--- src/task_with_letters/Letters.java
+++ src/task_with_letters/Letters.java
@@ -3,4 +3,12 @@ package task_with_letters;
 public class Letters {
     Integer number;
     String letter;
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public String getLetter() {
+        return letter;
+    }
 }
