917ae762346eb3db7b74d871a7aaf0cd2718e87d
MichalGuz <michalguz@gmail.com>
2019-05-20 08:15:14 +0200

    Create the body of method ArrayListCreator.

--- src/time_of_operation/TimeComparison.java
+++ src/time_of_operation/TimeComparison.java
@@ -5,35 +5,6 @@ import java.util.LinkedList;
 
 public class TimeComparison {
     public static void main(String[] args) {
-        ArrayList<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i < 1000000; i++) {
-            numbers.add(i);
-        }
-
-        ArrayListProcessor arrayListProcessor = new ArrayListProcessor();
-        arrayListProcessor.timeOfAddFirstToArrayList(numbers);
-        arrayListProcessor.timeOfAddLastToArrayList(numbers);
-        arrayListProcessor.timeOfRemoveFirstFromArrayList(numbers);
-        arrayListProcessor.timeOfRemoveLastFromArrayList(numbers);
-        arrayListProcessor.timeOfGetFromMiddleOfArrayList(numbers);
-
-        ArrayListPrintTime arrayListPrintTime = new ArrayListPrintTime();
-        arrayListPrintTime.printArrayList(arrayListProcessor,numbers);
-
-        ArrayList<Integer> numbers2 = new ArrayList<>();
-        for (int i = 0; i < 1000; i++) {
-            numbers2.add(i);
-        }
-
-        System.out.println("\n");
-
-        arrayListProcessor.timeOfAddFirstToArrayList(numbers2);
-        arrayListProcessor.timeOfAddLastToArrayList(numbers2);
-        arrayListProcessor.timeOfRemoveFirstFromArrayList(numbers2);
-        arrayListProcessor.timeOfRemoveLastFromArrayList(numbers2);
-        arrayListProcessor.timeOfGetFromMiddleOfArrayList(numbers2);
-
-        arrayListPrintTime.printArrayList(arrayListProcessor,numbers2);
 
         LinkedListCreator linkedListCreator = new LinkedListCreator();
         linkedListCreator.linkedListCreate();
