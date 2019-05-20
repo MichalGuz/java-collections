e1284dd608be265b7abd48750e79f7520ce06d5f
MichalGuz <michalguz@gmail.com>
2019-05-20 08:17:21 +0200

    Delete unnecessary lines of code. Create new object of class ArrayListCreator for executing method arrayListCreate().

--- src/time_of_operation/TimeComparison.java
+++ src/time_of_operation/TimeComparison.java
@@ -6,6 +6,9 @@ import java.util.LinkedList;
 public class TimeComparison {
     public static void main(String[] args) {
 
+        ArrayListCreator arrayListCreator = new ArrayListCreator();
+        arrayListCreator.arrayListCreate();
+
         LinkedListCreator linkedListCreator = new LinkedListCreator();
         linkedListCreator.linkedListCreate();
     }
