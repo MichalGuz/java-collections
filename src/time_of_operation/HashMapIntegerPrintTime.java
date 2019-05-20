dc0a76b936d3ac3cd5d98012daf5eb559e42c325
MichalGuz <michalguz@gmail.com>
2019-05-20 23:50:32 +0200

    Create new class with methods for printing the results of operating on hashMap without override hashCode()

---
+++ src/time_of_operation/HashMapIntegerPrintTime.java
@@ -0,0 +1,16 @@
+package time_of_operation;
+
+import java.util.HashMap;
+
+public class HashMapIntegerPrintTime {
+    public void printHashMapIntegerTime(HashMapIntegerProcessor hashMapIntegerProcessor, HashMap<Integer, String> hashMap){
+        System.out.println("HashMap contains: " + hashMap.size() + " elements.");
+        System.out.println("Time of add a new item with an index 0 to the HashMap containing " + hashMap.size() + " elements: " + hashMapIntegerProcessor.timeOfAddOneItemToHashMapInteger(hashMap) + "[ns];");
+        System.out.println("Time of adding new item with the highest index in the collection containing " + hashMap.size() + " elements: " + hashMapIntegerProcessor.timeOfAddAnotherItemToHashMapInteger(hashMap) + "[ns];");
+        System.out.println("Time of removing an item with index 0 from the collection containing " + hashMap.size() + " elements: "  + hashMapIntegerProcessor.timeOfRemoveOneItemFromHashMapInteger(hashMap) + "[ns];");
+        System.out.println("Time of removing an item with the highest index in the collection containing " + hashMap.size() + " elements: " + hashMapIntegerProcessor.timeOfRemoveAnotherItemFromHashMapInteger(hashMap) + "[ns];");
+        System.out.println("Time of getting an item with index equals a half size of the collection containing " + hashMap.size() + " elements:" + hashMapIntegerProcessor.timeOfGetAnotherItemFromHashMapInteger(hashMap));
+        System.out.println("After all operations HashMap contains: " + hashMap.size() + " elements.");
+    }
+}
+
