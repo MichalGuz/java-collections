a498cffd3163d390b38494a1351a69accc69f526
MichalGuz <michalguz@gmail.com>
2019-05-22 23:25:18 +0200

    Create methods of romoving new items in collection

--- src/time_of_operation/HashSetProcessor.java
+++ src/time_of_operation/HashSetProcessor.java
@@ -12,9 +12,27 @@ public class HashSetProcessor {
         return differenceOfTime = end1 - start1;
     }
 
-    public long timeOfAddAnotherItemToHashSet(HashSet<ItemForSet> hashSet){
+    public long timeOfAddAnotherItemToHashSet(HashSet<ItemForSet> hashSet) {
         long start1 = System.nanoTime();
         hashSet.add(new ItemForSet(1000001));
         long end1 = System.nanoTime();
         return differenceOfTime = end1 - start1;
+    }
+
+    public long timeOfRemoveOneItemFromHashSet(HashSet<ItemForSet> hashSet) {
+        ItemForSet item = new ItemForSet(0);
+        long start1 = System.nanoTime();
+        hashSet.remove(item);
+        long end1 = System.nanoTime();
+        return differenceOfTime = end1 - start1;
+    }
+
+    public long timeOfRemoveAnotherItemFromHashSet(HashSet<ItemForSet> hashSet) {
+        ItemForSet anotherItem = new ItemForSet(1000000);
+        long start1 = System.nanoTime();
+        hashSet.remove(anotherItem);
+        long end1 = System.nanoTime();
+        return differenceOfTime = end1 - start1;
+
+
 }
