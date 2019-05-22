29db201ce5a5f3880db7c410b401e5e29275e138
MichalGuz <michalguz@gmail.com>
2019-05-22 23:20:26 +0200

    Create methods of adding new items in collection

--- src/time_of_operation/HashSetProcessor.java
+++ src/time_of_operation/HashSetProcessor.java
@@ -1,5 +1,20 @@
 package time_of_operation;
 
+import java.util.HashSet;
+
 public class HashSetProcessor {
     long differenceOfTime;
+
+    public long timeOfAddOneItemToHashSet(HashSet<ItemForSet> hashSet){
+        long start1 = System.nanoTime();
+        hashSet.add(new ItemForSet(0));
+        long end1 = System.nanoTime();
+        return differenceOfTime = end1 - start1;
+    }
+
+    public long timeOfAddAnotherItemToHashSet(HashSet<ItemForSet> hashSet){
+        long start1 = System.nanoTime();
+        hashSet.add(new ItemForSet(1000001));
+        long end1 = System.nanoTime();
+        return differenceOfTime = end1 - start1;
 }
