2d5b2b4e247015bc136ca71183b23df8468ebf9d
MichalGuz <michalguz@gmail.com>
2019-05-23 00:18:58 +0200

    Create new class with method void for executing all methods from class HashSetProcessor and HashSetPrintTime

---
+++ src/time_of_operation/HashSetCreator.java
@@ -0,0 +1,27 @@
+package time_of_operation;
+
+import java.util.HashSet;
+
+public class HashSetCreator {
+    public void arrayListCreate() {
+
+        HashSetProcessor hashSetProcessor = new HashSetProcessor();
+        HashSetPrintTime hashSetPrintTime = new HashSetPrintTime();
+
+        HashSet<ItemForSet> items = new HashSet<>();
+        for (int i = 0; i < 10000; i++) {
+            items.add(new ItemForSet(i));
+        }
+
+        hashSetProcessor.timeOfAddOneItemToHashSet(items);
+        hashSetProcessor.timeOfAddAnotherItemToHashSet(items);
+        hashSetProcessor.timeOfRemoveOneItemFromHashSet(items);
+        hashSetProcessor.timeOfRemoveAnotherItemFromHashSet(items);
+        hashSetProcessor.timeOfCheckIfHashSetContains(items);
+
+        hashSetPrintTime.printHashSetTime(hashSetProcessor, items);
+
+
+
+    }
+}
