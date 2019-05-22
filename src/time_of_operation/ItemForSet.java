de5953c919c77360e957bbfc18f1ef1c0207e443
MichalGuz <michalguz@gmail.com>
2019-05-22 23:12:38 +0200

    Override method equals()

--- src/time_of_operation/ItemForSet.java
+++ src/time_of_operation/ItemForSet.java
@@ -6,4 +6,10 @@ public class ItemForSet {
     ItemForSet(Integer item){
         this.item = item;
     }
+
+    @Override
+    public boolean equals(Object o){
+        final ItemForSet i = (ItemForSet) o;
+        return this.item.equals(i.item);
+    }
 }
