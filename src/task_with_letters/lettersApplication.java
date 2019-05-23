5edcb9e7cfbaa87cbdd286ab53ce06292505274b
MichalGuz <michalguz@gmail.com>
2019-05-23 23:58:17 +0200

    Create new object of class LettersArrayDeque and execute the method createArrayDeque()

--- src/task_with_letters/lettersApplication.java
+++ src/task_with_letters/lettersApplication.java
@@ -1,5 +1,8 @@
 package task_with_letters;
 
 public class lettersApplication {
-
+    public static void main(String[] args) {
+        LettersArrayDeque lettersArrayDeque = new LettersArrayDeque();
+        lettersArrayDeque.createArrayDequeWithLetters();
+    }
 }
